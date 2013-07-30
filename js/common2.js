79effb4fdcf2163174aba513d6eb2aaca87b531c
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2013-07-30 09:02:06 -0700

    Multiple reply support

--- js/common2.js
+++ js/common2.js
@@ -1,6 +1,9 @@
 // JavaScript Document
 
+// Configuration
 var bPlaceHolderEdited = false;
+var fReplyDelay = 1750; // ms
+var iReplyQueue = 0;
 
 function addEventList(content) {
 	var li = document.createElement('li');
@@ -26,6 +29,17 @@ function addEventList(content) {
 	return li;
 }
 
+function reply(content) {
+	iReplyQueue++;
+	var newli = addEventList(content);
+	$(newli).hide();
+	$(newli).delay(iReplyQueue*fReplyDelay).fadeIn();
+	
+	setTimeout(function() {
+		iReplyQueue--;
+	}, iReplyQueue*fReplyDelay);
+}
+
 $(document).ready(function(e) {
     $("#compose").click(function(e) {
 		if (!bPlaceHolderEdited) $(this).html("");
@@ -35,9 +49,8 @@ $(document).ready(function(e) {
 	$("#compose").keypress(function(e) {
         if (e.charCode == 13) {
 			addEventList($(this).html());
-			var newli = addEventList("Here's <a href=\"#\">Notebook</a>");
-			$(newli).hide();
-			$(newli).delay(1750).fadeIn();
+			reply("Here's <a href=\"#\">Notebook</a>, first reply");
+			reply("Here's <a href=\"#\">Notebook</a>, second reply");
 		}
     });
 });
